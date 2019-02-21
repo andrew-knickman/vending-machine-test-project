b8a453997ecb390d156653377348c75db7dbeff8
Andrew Knickman <andrewsknick@gmail.com>
2019-02-20 23:58:46 -0500

    Project 2 commit - Vending Machine code with documentation on tests

--- VendingMachine/src/edu/towson/cis/cosc442/project2/vendingmachine/VendingMachineTest.java
+++ VendingMachine/src/edu/towson/cis/cosc442/project2/vendingmachine/VendingMachineTest.java
@@ -11,18 +11,21 @@ public class VendingMachineTest {
 	VendingMachine vm;
 	VendingMachineItem vmi;
 	
+	/**Constructs test VendingMachine and VendingMachineItem objects*/
 	@Before
 	public void setUp() throws Exception {
 		vm = new VendingMachine();
 		vmi = new VendingMachineItem("Test Item", 1.99);
 	}
-
+	
+	/**Cleans up test objects*/
 	@After
 	public void tearDown() throws Exception {
 		vm = null;
 		vmi = null;
 	}
 
+	/**Tests if addItem method adds VendingMachineItem and adds it correctly to VendingMachine*/
 	@Test
 	public void testAddItem() {
 		vm.addItem(vmi, "A");
@@ -30,7 +33,8 @@ public class VendingMachineTest {
 		assertEquals(1.99,vm.getItem("A").getPrice(),0.001);
 		assertTrue(vm.getItem("A") instanceof VendingMachineItem);
 	}
-
+	
+	/**Tests if removeItem method removes correct VendingMachineItem removes it correctly from VendingMachine*/
 	@Test
 	public void testRemoveItem() {
 		vm.addItem(vmi, "A");
@@ -40,6 +44,7 @@ public class VendingMachineTest {
 		assertTrue(removed instanceof VendingMachineItem);
 	}
 
+	/**Tests if insertMoney method adds correct value to VendingMachine balance*/ 
 	@Test
 	public void testInsertMoney() {
 		vm.insertMoney(1.00);
@@ -47,7 +52,8 @@ public class VendingMachineTest {
 		vm.balance = 0.0;
 		assertEquals(0.00,vm.balance,0.001);
 	}
-
+	
+	/**Tests is getBalance method returns correct balance of VendingMachine*/
 	@Test
 	public void testGetBalance() {
 		vm.insertMoney(1.00);
@@ -56,6 +62,7 @@ public class VendingMachineTest {
 		assertEquals(0.00,vm.balance,0.001);
 	}
 
+	/**Tests if makePurchase is able to purchase valid item from VendingMachine*/
 	@Test
 	public void testMakePurchase() {
 		vm.balance = 2.00;
@@ -65,6 +72,7 @@ public class VendingMachineTest {
 		assertEquals(0.00,vm.balance,0.001);
 	}
 
+	/**Tests if returnChange method returns correct change value*/
 	@Test
 	public void testReturnChange() {
 		vm.balance = 0.01;
